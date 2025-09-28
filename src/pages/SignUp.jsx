54029216e74a7731ceabf118c2403bc276db13be
Rahim <zowangrm@mail.uc.edu>
2025-09-28 19:37:50 -0400

    Created Signin feature

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,9 +1,55 @@
 import React from 'react'
+import { useState } from 'react'
+import { FaEye,FaEyeSlash } from "react-icons/fa";
+import { Link } from 'react-router';
+import OAuth from '../components/OAuth.jsx';
 
 export default function SignUp() {
+
+const [showPassword, setShowPassword] = useState(false);
+
+const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password:"",
+});
+
+function onChange(e){
+    setFormData((prevState) => ({
+        ...prevState,
+         [e.target.id]: e.target.value,
+}))
+}
+
   return (
-    <div>
-      SignUp
-    </div>
+    <section>
+        <h1 className="text-3xl text-center mt-6 font-bold ">Sign Up</h1>
+        <div className="flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl mx-auto"> 
+            <div className="md:w-[67%] lg:w-[50%] mb-12 md:mb-6">
+               <img src="https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8a2V5fGVufDB8fDB8fHww" alt="key" className="w-full rounded-2xl"/>
+            </div>
+            <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
+               <form> 
+                <input className="w-full text-xl mb-4 text-gray-700 bg-white border border-gray-300 rounded px-2 py-0.5 transition ease-in-out" type="text" id="name" placeholder="Full Name" value={formData.name} onChange={onChange}/>
+                <input className="w-full text-xl mb-4 text-gray-700 bg-white border border-gray-300 rounded px-2 py-0.5 transition ease-in-out" type="email" id="email" placeholder="Email Address" value={formData.email} onChange={onChange}/>
+                <div className="relative mb-6">         
+                    <input className="w-full text-xl text-gray-700 bg-white border border-gray-300 rounded px-2 py-0.5 transition ease-in-out" type={showPassword ? "text" : "password"} id="password" placeholder="Password" value={formData.password} onChange={onChange}/>
+                      {showPassword ? <FaEye className="absolute right-3 top-1.5 text-xl cursor-pointer" onClick={() => setShowPassword((prevState) => !prevState)}/> : <FaEyeSlash className="absolute right-3 top-1.5 text-xl cursor-pointer" onClick={() => setShowPassword((prevState) => !prevState )}/>}
+                </div>
+                <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg">
+                    <p className="mb-6">Have an account? <Link className="text-red-600 hover:text-red-700 transition duration-200 ease-in-out" to="/sign-in">Sign In</Link></p>
+                    <p><Link className="text-blue-600 hover:text-blue-700 transition duration-200 ease-in-out" to="/forgot-password">Forgot password</Link></p>
+                </div>
+                  <button className="w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800" type="submit">Sign up</button>
+               <div className="flex items-center my-4
+               before:border-t before:flex-1 before:border-gray-300
+               after:border-t after:flex-1 after:border-gray-300">
+                <p className="text-center font-semibold mx-4">OR</p>
+               </div>
+               <OAuth/>
+               </form>
+            </div>
+        </div>
+    </section>
   )
 }
