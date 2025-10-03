fd501426404998001cba818b5606156b3d392426
Rahim <zowangrm@mail.uc.edu>
2025-10-02 20:41:35 -0400

    Added delete functionality

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -9,7 +9,14 @@ import { db } from "../firebase.js";
 import { FcHome } from "react-icons/fc";
 import { Link } from "react-router-dom";
 import ListingItem from "../components/ListingItem.jsx";
-import { collection, query, where, getDocs, orderBy } from "firebase/firestore";
+import {
+  collection,
+  query,
+  where,
+  getDocs,
+  orderBy,
+  deleteDoc,
+} from "firebase/firestore";
 
 export default function Profile() {
   const auth = getAuth();
@@ -80,6 +87,22 @@ export default function Profile() {
 
   console.log(listings);
 
+  async function onDelete(listingID) {
+    if (window.confirm("Are you sure you want to delete?")) {
+      await deleteDoc(doc(db, "listings", listingID));
+      const updatedListings = listings.filter(
+        (listing) => listing.id !== listingID
+      );
+      console.log(updatedListings);
+      setListings(updatedListings);
+      toast.success("Successfully deleted the listing");
+    }
+  }
+
+  function onEdit(listingID) {
+    navigate(`/edit-listing/${listingID}`);
+  }
+
   return (
     <div>
       <section className="max-w-6xl mx-auto flex justify-center items-center flex-col">
@@ -145,12 +168,14 @@ export default function Profile() {
         {!loading && listings.length > 0 && (
           <>
             <h2 className="text-2xl text-center font-semibold">My Listings</h2>
-            <ul>
+            <ul className="sm:grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl-grid-cols-5 mt-6 mb-6">
               {listings.map((listing) => (
                 <ListingItem
                   key={listing.id}
                   id={listing.id}
                   listing={listing.data}
+                  onDelete={() => onDelete(listing.id)}
+                  onEdit={() => onEdit(listing.id)}
                 />
               ))}
             </ul>
